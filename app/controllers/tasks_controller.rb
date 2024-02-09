a91c75e3c6108d0c69b61382a4ae0bd6e8f7e7d5
Pauline Wangunyu <paulinewangunyu@Paulines-MacBook-Air.local>
2024-02-09 15:30:24 +0000

    Allow user to add tasks

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -1,5 +1,22 @@
 class TasksController < ApplicationController
+  skip_before_action :verify_authenticity_token, :only => [:create,:inquire_enterprise]
   def index
     @tasks = Task.all
   end
+
+  def create
+    task = Task.new(task_params)
+    if task.save 
+      redirect_to tasks_path, notice: 'Task created'
+    else 
+      render json: { error: todo.errors.full_messages.join(', ') }, status: :unprocessable_entity
+    end
+  end
+
+  private
+
+  def task_params
+    params.require(:task).permit(:description)
+  end
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -12,7 +12,6 @@ end
 Rails.application.routes.draw do
   root "tasks#index"
   get "/tasks", to: "tasks#index"
-
-  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  post "/tasks", to: "tasks#create"
 end
 
